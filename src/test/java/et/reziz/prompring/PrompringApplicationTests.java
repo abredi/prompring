34611e5ca69c22807fa2b84649a2375665ea4231
Abdulaziz Ali <rediabdulaziz@gmail.com>
2022-07-11 11:14:52 -0500

    init

---
+++ src/test/java/et/reziz/prompring/PrompringApplicationTests.java
@@ -0,0 +1,13 @@
+package et.reziz.prompring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrompringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
